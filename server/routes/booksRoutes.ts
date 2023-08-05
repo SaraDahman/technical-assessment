db0c41c19453b9c7ebb1f46380437aac25bbe737
unknown <saritadh74@gmail.com>
2023-08-05 17:15:35 +0300

    used the check auth middleware with books routes

--- server/routes/booksRoutes.ts
+++ server/routes/booksRoutes.ts
@@ -1,12 +1,20 @@
 import express from 'express';
-import { addOneBook, getAllBooks, deleteBook, updateBook, filterBooks } from '../controllers/booksController';
+import {
+    addOneBook,
+    getAllBooks,
+    deleteBook,
+    updateBook,
+    filterBooks,
+    getOneBook
+} from '../controllers/booksController';
 
 const router = express.Router();
 
 router.post('/', addOneBook);
 router.get('/', getAllBooks);
-router.get('/filter', filterBooks);
+router.get('/:id', getOneBook);
 router.delete('/:id', deleteBook);
 router.put('/:id', updateBook);
+router.get('/filter', filterBooks);
 
 export default router;
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import authRouter from './authRoutes';
 import booksRouter from './booksRoutes';
+import { checkAuth } from '../middleware';
 
 const router = express.Router();
 
 router.use('/auth', authRouter);
-router.use('/books', booksRouter);
+router.use('/books', checkAuth, booksRouter);
 
 
 export default router;
