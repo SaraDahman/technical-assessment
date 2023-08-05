7029ff9ae73248ab269a3b602aafa0141224c3ec
unknown <saritadh74@gmail.com>
2023-08-05 23:21:11 +0300

    handeled getting one book relates #8

--- server/controllers/booksController.ts
+++ server/controllers/booksController.ts
@@ -4,46 +4,62 @@ import booksService from "../services/booksService";
 import { CustomError } from "../helpers";
 import { IRequest } from "../interfaces";
 
-export const addOneBook = async (req: any, res: Response, next: NextFunction) => {
-    try {
-        const userId = req.user.id;
-        const data = await addBookValidation(req.body);
-        const response = await booksService.addOneBook(data, userId);
+export const addOneBook =
+    async (req: any, res: Response, next: NextFunction) => {
+        try {
+            const userId = req.user.id;
+            const data = await addBookValidation(req.body);
+            const response = await booksService.addOneBook(data, userId);
 
-        res.status(201).json(response);
+            res.status(201).json(response);
 
-    } catch (error: any) {
-        if (error.name === 'ValidationError') next(new CustomError(400, error.message));
-        else next(error);
-    }
-}
-
-export const getAllBooks = async (req: any, res: Response, next: NextFunction) => {
-    try {
-        const userId = req.user.id;
-        const response = await booksService.getAllBooks(userId);
-
-        res.status(200).send(response);
-
-    } catch (error) {
-        next(error);
+        } catch (error: any) {
+            if (error.name === 'ValidationError') next(new CustomError(400, error.message));
+            else next(error);
+        }
     }
 
-}
+export const getAllBooks =
+    async (req: any, res: Response, next: NextFunction) => {
+        try {
+            const userId = req.user.id;
+            const response = await booksService.getAllBooks(userId);
 
-export const getOneBook = async (req: any, res: Response) => {
-    res.send('this is get one book')
-}
+            res.status(200).send(response);
 
-export const deleteBook = async (req: any, res: Response) => {
-    res.send('this is delete a book')
-}
+        } catch (error) {
+            next(error);
+        }
 
-export const updateBook = async (req: any, res: Response) => {
-    res.send('this is update a book')
-}
+    }
 
-export const filterBooks = async (req: any, res: Response) => {
-    res.send('this is search book')
-}
+export const getOneBook =
+    async (req: any, res: Response, next: NextFunction) => {
+        try {
+            const bookId = req.params.id;
+            const response = await booksService.getOneBook(bookId);
+
+            res.status(200).json(response);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+
+export const deleteBook =
+    async (req: any, res: Response, next: NextFunction) => {
+        res.send('this is delete a book')
+    }
+
+
+export const updateBook =
+    async (req: any, res: Response, next: NextFunction) => {
+        res.send('this is update a book')
+    }
+
+
+export const filterBooks =
+    async (req: any, res: Response, next: NextFunction) => {
+        res.send('this is search book')
+    }
 
--- server/repositories/booksRepository.ts
+++ server/repositories/booksRepository.ts
@@ -1,20 +1,35 @@
 import { Book } from "../database/models";
+import { Op } from "sequelize";
 
 export const createBook = async (newBook: any) => {
     return await Book.create(newBook);
 }
 
-export const findBookByTitle = async (title: string) => {
-    return await Book.findOne({
-        where: { title },
-    });
-}
 
 export const findAllBooks = async (userId: number) => {
     return await Book.findAll({
         where: { userId },
     });
 }
+
+
+export const findBookById = async (id: number) => {
+    return await Book.findOne({
+        where: { id },
+    });
+}
+
+
+export const findBookByTitle = async (title: string) => {
+    return await Book.findOne({
+        where: {
+            title: {
+                [Op.like]: `${title}%`
+            }
+        },
+    });
+}
+
 
 export const deleteBookById = async (id: number) => {
 }
