50fbcd7b195bf3e458e683ea7b73deeb4a18b67b
unknown <saritadh74@gmail.com>
2023-08-06 01:57:28 +0300

    added a helper function that validates the params

--- server/helpers/index.ts
+++ server/helpers/index.ts
@@ -1,3 +1,4 @@
 export { default as CustomError } from './CustomError';
 export { default as generateToken } from './generateToken';
 export { default as verifyToken } from './verifyToken';
+export { default as validateParams } from './validateParams';
