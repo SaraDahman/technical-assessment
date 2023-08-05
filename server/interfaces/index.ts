d9be9ff2dd1d315ae4c299f63ebeb9112bc06bcf
unknown <saritadh74@gmail.com>
2023-08-05 17:17:20 +0300

    added new interfaces

--- server/interfaces/index.ts
+++ server/interfaces/index.ts
@@ -1,2 +1,5 @@
 export { default as ISignup } from './ISignUp';
 export { default as ISignIn } from './ISignIn';
+export { default as IBook } from './IBook';
+export { default as IUserPayload } from './IUserPayload';
+export { default as IRequest } from './IRequest';
