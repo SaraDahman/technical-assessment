a49cc3af7ab59c6d2a1bd7ddf587af33e5552028
unknown <saritadh74@gmail.com>
2023-08-07 02:51:24 +0300

    removed unneeded files, added new ones, installed packages, and made all nessecary changes relates #16

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ],
+  devServer:{
+    proxy: {
+      '/api' : {
+        target : 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
