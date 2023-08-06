51886a93847b4aa8a6471a411341f4567ffe2c29
unknown <saritadh74@gmail.com>
2023-08-06 03:09:46 +0300

    removed repetitive code

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    res.status(err.status || 500)
+    if (err.name === 'ValidationError') return res.status(400).json({ message: err.message });
+    else res.status(err.status || 500)
         .json({ message: err.message || 'Internal Server Error' });
 }
 
