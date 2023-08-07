ca51ffdfc5fb1aff4b713b7215ca14e989a48099
unknown <saritadh74@gmail.com>
2023-08-07 19:49:04 +0300

    added the dashbord page and made the foms button load when the request isnt done relates #16

---
+++ client/src/views/Dashboard.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-card style="height: 100vh">
+    <v-layout>
+      <v-navigation-drawer
+        v-model="drawer"
+        :mini-variant.sync="mini"
+        permanent
+        style="height: 100vh"
+        sticky
+      >
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-title>John Leider</v-list-item-title>
+
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            @click="navigate(item.path)"
+          >
+            <v-list-item-icon>
+              <v-icon color="#00ACC1">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <template v-slot:append>
+          <v-list dense>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon color="#00ACC1">mdi-logout-variant</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>LOG OUT</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <!-- <div class="pa-2">
+            <v-btn block color="#4DD0E1"> Logout </v-btn>
+          </div> -->
+        </template>
+      </v-navigation-drawer>
+
+      <v-main>
+        <router-view />
+      </v-main>
+    </v-layout>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  data: () => ({
+    drawer: true,
+    mini: true,
+    items: [
+      {
+        title: 'My Books',
+        icon: 'mdi-book-open-page-variant',
+        value: 'Books',
+        path: 'books',
+      },
+      {
+        title: 'Add a Book',
+        icon: 'mdi-pen-plus',
+        value: 'add book',
+        path: 'add-book',
+      },
+      {
+        title: 'Archived Books',
+        icon: 'mdi-delete',
+        value: 'users',
+        path: 'archived',
+      },
+    ],
+  }),
+  methods: {
+    async navigate(path: string) {
+      try {
+        await this.$router.push({ name: path });
+      } catch (error) {
+        this.$toast('already in this page');
+      }
+    },
+  },
+});
+</script>
