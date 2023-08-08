a15a24becb3e86db99bce07bb20bbbbdf1093f08
unknown <saritadh74@gmail.com>
2023-08-08 21:21:44 +0300

    added interfaces relates #22

---
+++ client/src/interfaces/IBook.ts
@@ -0,0 +1,11 @@
+export interface IBook {
+    id: number;
+    createdAt: string;
+    updatedAt: string;
+    deletedAt: string
+    title: string;
+    author: string;
+    description: string;
+    category: string;
+    image: string;
+}
