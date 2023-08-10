6a9f86a49b92e3b82e29c66482412d54a57b9c49
unknown <saritadh74@gmail.com>
2023-08-10 16:53:18 +0300

    added a not found page

---
+++ client/src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="not-found">
+    <h2>page bot found ...</h2>
+    <v-btn @click="() => $router.push('/')"> Go Home </v-btn>
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
+
+<style>
+.not-found {
+  text-align: center;
+  padding: 4rem 0;
+}
+
+.not-found h2 {
+  margin-bottom: 2rem;
+}
+</style>
