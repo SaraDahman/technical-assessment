2bb68f9ccca58d80071bbc2a85de568d58ecd8c0
unknown <saritadh74@gmail.com>
2023-08-10 16:52:08 +0300

    used a dialog for delettion confirmation relates #28

---
+++ client/src/components/Dialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-dialog :value="dialog" persistent width="500">
+    <v-card>
+      <v-card-title class="text-h5"> Delete Warning</v-card-title>
+      <v-card-text>
+        Are you sure you want to delete this item? This action cannot be undone.
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn variant="text" @click="closeDialog"> No </v-btn>
+        <v-btn variant="text" @click="deleteBook" color="warning"> Yes </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: ['dialog', 'closeDialog', 'deleteBook'],
+  data() {
+    return {
+      isVisible: false,
+    };
+  },
+});
+</script>
